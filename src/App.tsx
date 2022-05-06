55753b1cdbbdf03a2ed9d2a4a04eb68e2537a61e
Akita Emmanuel <e.akita91@gmail.com>
2022-05-06 06:49:24 +0100

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { ProvideSideBarContext, ProvideThemeContext } from "hooks";
+import { Layout } from "components";
+import { Home } from "pages";
+import moment from "moment";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <BrowserRouter>
+      <ProvideThemeContext>
+        <ProvideSideBarContext>
+          <Layout>
+            <Routes>
+            <Route path='/' element={<Home />} />
+            </Routes>
+          </Layout>
+        </ProvideSideBarContext>
+      </ProvideThemeContext>
+  </BrowserRouter>
+)
