3c6c2a70489baa8b8ba25363b450f43a83348e04
Akita Emmanuel <e.akita91@gmail.com>
2022-05-11 01:03:10 +0100

    Deleted redundant code

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,32 +1,13 @@
 import React from "react";
-import { MainPanel, CreateNoteFAB, ProductLists } from "components";
-import { createStyles, Typography, makeStyles } from "@material-ui/core";
+import { MainPanel, ProductLists } from "components";
 
-const useStyles = makeStyles((theme) => createStyles({
-  content: {
-    display: 'flex',
-    flexDirection: "column",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    "& h4": {
-      textAlign: "center"
-    }
-  }
-}))
+
 
 export const Products: React.FC = () => {
-  const classes = useStyles();
 
   return (
     <MainPanel>
-      {/* <div className={classes.content}> */}
-        {/* <Typography variant="h4" color="textSecondary">
-          Select a note to get started
-        </Typography> */}
         <ProductLists />
-      {/* </div> */}
-      <CreateNoteFAB />
     </MainPanel>
   )
 }
