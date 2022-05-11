dda40b4e1efe22490436d384a49850ea5083dd7b
Akita Emmanuel <e.akita91@gmail.com>
2022-05-11 01:06:38 +0100

    Added mobile menu

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,70 +1,159 @@
 import React from "react";
-import {
-  AppBar,
-  Toolbar,
-  CssBaseline,
-  Typography,
-  makeStyles,
-  createStyles
-} from "@material-ui/core";
+import CssBaseline from '@mui/material/CssBaseline';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { createTheme } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      color: "#363738"
-    },
-    RightNavLinks: {
-      marginLeft: theme.spacing(5),
-      display: "flex",
-    },
-    LeftLinks: {
-      marginLeft: theme.spacing(10),
-    },
-    LeftNavLinks: {
-      flexGrow: 1,
-      cursor: "pointer",
-    },
-    link: {
-      textDecoration: "none",
-      color: "#363738",
-      fontSize: "16px",
-      marginLeft: theme.spacing(3),
-      "&:hover": {
-        borderBottom: "2px solid #363b2f",
-      },
-    },
-  })
-)
+const theme = createTheme()
+const menus = ['Shop', 'Learn', 'Account',];
 
 export const Appbar: React.FC = () => {
-  const classes = useStyles();
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+ 
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <AppBar position="static" color="default">
+    <AppBar
+      position="static"
+      color="default"
+      sx={{
+        color: "#363738",
+        display: "flex",
+      }}
+    >
       <CssBaseline />
-      <Toolbar>
-        <Typography variant="h4" color="inherit" className={classes.LeftNavLinks}>
+      <Toolbar
+        sx={{
+          color: "#363738",
+          flexDirection: 'row',
+        }}
+      >
+        <Typography
+          variant="h4"
+          color="inherit"
+          sx={{
+            cursor: "pointer",
+            display: "flex",
+            flexGrow: 1,
+          }}>
           L U M I N
-          <span className={classes.LeftLinks}>
-            <Link to="/shop" className={classes.link}>
+          <Typography 
+            sx={{
+              marginLeft: theme.spacing(10),
+              display: "none",
+              [theme.breakpoints.up("lg")]: {
+                display: "flex"
+              }
+            }}
+          >
+            <Link
+              to="/shop"
+
+              style={{
+                textDecoration: "none",
+                color: "#363738",
+                fontSize: "16px",
+                marginLeft: theme.spacing(3),
+              }}
+            >
               Shop
             </Link>
-            <Link to="/learn" className={classes.link}>
+            <Link
+              to="/learn"
+
+              style={{
+                textDecoration: "none",
+                color: "#363738",
+                fontSize: "16px",
+                marginLeft: theme.spacing(3),
+              }}
+            >
               Learn
             </Link>
-          </span>
+          </Typography>
         </Typography>
-        <div className={classes.RightNavLinks}>
-          <Link to="/account" className={classes.link}>
+        <Typography
+          sx={{
+            marginLeft: theme.spacing(5),
+            display: "none",
+            [theme.breakpoints.up("lg")]: {
+              display: 'flex'
+            }
+          }}
+        >
+          <Link
+            to="/account"
+            style={{
+              textDecoration: "none",
+              color: "#363738",
+              fontSize: "16px",
+              marginLeft: theme.spacing(3),
+            }}
+          >
             Account
           </Link>
-          <Link to="/checkout" className={classes.link}>
+          <Link
+            to="/checkout"
+            style={{
+              textDecoration: "none",
+              color: "#363738",
+              fontSize: "16px",
+              marginLeft: theme.spacing(3),
+            }}
+            >
             <ShoppingCartIcon color="inherit" aria-label="Shopping Cart"/>
           </Link>
-        </div>
+        </Typography>
+        <Box sx={{
+          flexGrow: 0,
+          marginLeft: theme.spacing(20),
+          [theme.breakpoints.up("lg")]: {
+            display: "none"
+          }
+        }}>
+          <Tooltip title="Open Menu">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <MenuIcon />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {menus.map((menu) => (
+              <MenuItem key={menu} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{menu}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
       </Toolbar>
     </AppBar>
   );
