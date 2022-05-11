3cf0344dad93f93c770a1dc00a91bebe94cfb4f6
Akita Emmanuel <e.akita91@gmail.com>
2022-05-11 01:06:07 +0100

    Added currency symbol

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {Product} from 'components'
 
@@ -11,10 +9,11 @@ interface Props {
   item: Product,
   addToCart: (clickedItem: Product) => void,
   removeFromCart: (id: number) => void,
+  symbol: string
 };
 
 const theme = createTheme()
-export const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) => {
+export const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart, symbol }) => {
   return (
     <ThemeProvider theme={theme}>
       <main
@@ -22,26 +21,49 @@ export const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) =
         width: '100%',
         backgroundColor: "#c6c7c5",
         display: "flex",
+        flexDirection: 'column',
         justifyContent: "space-between",
         fontFamily: "Arial, Helvetica, sans-serif",
         borderBottom: "1px solid #363b2f",
         paddingBottom: "20px",
       }}>
-        <Typography
-          component="h5"
-          variant="h2"
-          gutterBottom
-        >
-          {item.title}
-        </Typography>
+        <Toolbar sx={{ flexGrow: 1, p: 2 }}>
+          <Typography
+            component="h5"
+            gutterBottom
+          >
+           {item.title}
+          </Typography>
+
+          <img
+            src={item.image_url}
+            alt={item.title}
+            style={{
+              maxWidth: "80px",
+              objectFit: "cover",
+              marginLeft: "40px",
+            }}
+          />
+        </Toolbar>
         <Typography
           sx={{
             display: "flex",
             justifyContent: "space-between",
+            px: 4
           }}
         >
-          <p>Price: ${item.price}</p>
-          <p>Total: ${item.amount} * ${item.price}</p>
+          <Typography
+            component="h5"
+            gutterBottom
+          >
+            Price: {symbol} {item.price}
+          </Typography>
+          <Typography
+            component="h5"
+            gutterBottom
+          >
+            Total: {symbol} {(item.amount * item.price).toFixed(2)}
+          </Typography>
         </Typography>
         <Typography
           sx={{
@@ -58,12 +80,17 @@ export const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) =
               border: "none",
               boxShadow: "none",
               bgcolor: "transparent",
-              fontSize: '12px'
+              fontSize: '30px',
+              "&:hover": {
+                border: "none",
+                bgcolor: "#363b2f",
+              },
+              px: 1
             }}
           >
             -
           </Button>
-          <p>{item.amount}</p>
+          <Typography component="h5">{item.amount}</Typography>
           <Button
             size="small"
             disableElevation
@@ -73,14 +100,18 @@ export const CartItem: React.FC<Props> = ({ item, addToCart, removeFromCart }) =
               border: "none",
               boxShadow: "none",
               bgcolor: "transparent",
-              fontSize: '12px'
+              fontSize: '30px',
+              "&:hover": {
+                border: "none",
+                bgcolor: "#363b2f",
+              },
+              px: 1
             }}
           >
             +
           </Button>
         </Typography>
       </main>
-      <img src={item.image_url} alt={item.title} />
     </ThemeProvider>
   );
 };
